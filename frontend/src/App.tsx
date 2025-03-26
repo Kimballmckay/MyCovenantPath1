cc06b8f1219fe355266b497158a0f7e6f5974969
brooklynburnham <167155861+brooklynburnham@users.noreply.github.com>
2025-03-26 09:47:23 -0600

    add <app /> to main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import './App.css';
+import GoalsPlans from './GoalsPlans';
+import MyCovenantPath from './MyCovenantPath';
+import MyProgress from './MyProgress';
+import MyStudies from './MyStudies';
 import Menu from './pages/Menu/Menu';
 
 function App() {
   return (
     <>
-      <Menu />
+      <Menu/>
+      <GoalsPlans />
+      <MyCovenantPath />
+      <MyProgress />
+      <MyStudies />
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,20 +2,10 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 // import App from "./App.tsx";
-import MyCovenantPath from './MyCovenantPath.tsx';
-import Menu from './pages/Menu/Menu.tsx';
-import GoalsPlans from './GoalsPlans.tsx';
-import MyProgress from './MyProgress.tsx';
-import MyStudies from './MyStudies.tsx';
-
-//import MyCovenantPath from "./MyCovenantPath.tsx";
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Menu/>
-    <GoalsPlans />
-    <MyCovenantPath />
-    <MyProgress />
-    <MyStudies />
+    <App />
   </StrictMode>
 );
