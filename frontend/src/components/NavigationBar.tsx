0c8414a426a4c306269e24ca0f534fffd65ebdb0
brooklynburnham <167155861+brooklynburnham@users.noreply.github.com>
2025-03-29 15:16:20 -0600

    navbar dynamically changing based on page!

--- frontend/src/components/NavigationBar.tsx
+++ frontend/src/components/NavigationBar.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import styles from "../pages/Menu/Menu.module.css";
+import styles from '../pages/Menu/Menu.module.css';
 import NavItem from './NavItem';
 
 const NavigationBar: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  
+
   const handleNavClick = (path: string) => {
     navigate(path);
   };
@@ -19,27 +19,32 @@ const NavigationBar: React.FC = () => {
         isActive={location.pathname === '/path'}
         containerClassName={styles.iconContainer}
         stateLayerClassName={styles.stateLayer2}
-        navItemClassName={styles.navitem1}
+        navItemClassName={`${styles.navitem1} ${location.pathname === '/MyCovenantPath' ? styles.activeNavItem : ''}`}
         onClick={() => handleNavClick('/MyCovenantPath')}
       />
 
       <NavItem
         iconSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/31a01464f0168d2b4386f2d59a9cbc9088c82a9478cfc73c03c7a67346192708?placeholderIfAbsent=true&apiKey=3fb7e4d952c749b1b4b816af6fe83375"
         label="Progress"
-        isActive={location.pathname === '/MyProgress.tsx' || location.pathname === '/progress'}
+        isActive={
+          location.pathname === '/MyProgress.tsx' ||
+          location.pathname === '/progress'
+        }
         containerClassName={styles.iconContainer2}
         stateLayerClassName={styles.stateLayer3}
-        navItemClassName={styles.navitem2}
+        navItemClassName={`${styles.navitem2} ${location.pathname === '/MyProgress' ? styles.activeNavItem : ''}`}
         onClick={() => handleNavClick('/MyProgress')}
       />
 
       <NavItem
         iconSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/15be6936d5ee80b6bf0828801aa326bf800c811d4d6ba3964e4da1c631b42a66?placeholderIfAbsent=true&apiKey=3fb7e4d952c749b1b4b816af6fe83375"
         label="Goals"
-        isActive={location.pathname === '/GoalsPlans' || location.pathname === '/goals'}
+        isActive={
+          location.pathname === '/GoalsPlans' || location.pathname === '/goals'
+        }
         containerClassName={styles.iconContainer3}
         stateLayerClassName={styles.stateLayer4}
-        navItemClassName={styles.navitem3}
+        navItemClassName={`${styles.navitem3} ${location.pathname === '/GoalsPlans' ? styles.activeNavItem : ''}`}
         onClick={() => handleNavClick('/GoalsPlans')}
       />
 
@@ -49,17 +54,19 @@ const NavigationBar: React.FC = () => {
         isActive={location.pathname === '/study'}
         containerClassName={styles.iconContainer4}
         stateLayerClassName={styles.stateLayer5}
-        navItemClassName={styles.navitem4}
+        navItemClassName={`${styles.navitem4} ${location.pathname === '/MyStudies' ? styles.activeNavItem : ''}`}
         onClick={() => handleNavClick('/MyStudies')}
       />
 
       <NavItem
         iconSrc="https://cdn.builder.io/api/v1/image/assets/TEMP/ab7785cb9536970a94b2768a2045a46835b65ad2ed50994533fabf488462b39c?placeholderIfAbsent=true&apiKey=3fb7e4d952c749b1b4b816af6fe83375"
         label="Menu"
-        isActive={location.pathname === '/Menu' || location.pathname === '/menu'}
+        isActive={
+          location.pathname === '/Menu' || location.pathname === '/menu'
+        }
         containerClassName={styles.iconContainer5}
         stateLayerClassName={styles.stateLayer6}
-        navItemClassName={styles.navitem5}
+        navItemClassName={`${styles.navitem5} ${location.pathname === '/Menu' ? styles.activeNavItem : ''}`}
         onClick={() => handleNavClick('/Menu')}
       />
     </nav>
