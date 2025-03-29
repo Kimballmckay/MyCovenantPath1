4f20ef91c87978cb345390de6408b4959805b6d5
prestonpark26 <153689632+prestonpark26@users.noreply.github.com>
2025-03-29 11:37:18 -0600

    Added files into the pages folder

---
+++ frontend/src/pages/myProgress/MyProgress.tsx
@@ -0,0 +1,27 @@
+import StatusBar from '../../components/StatusBar';
+import NavigationBar from '../../components/NavigationBar';
+import ChurchAttendance from './ChurchAttendance';
+import TopicsStudied from './TopicsStudied';
+import TempleAttendance from './TempleAttendance';
+import MenuHeader from '../../components/RealHeader';
+
+function MyProgress() {
+  return (
+    <main className="mx-auto w-full max-w-[480px]">
+      <div className="overflow-hidden w-full bg-white">
+        <StatusBar />
+        <MenuHeader />
+
+        <section className="flex flex-col items-start px-3.5 mt-8 w-full">
+          <ChurchAttendance />
+          <TopicsStudied />
+          <TempleAttendance />
+        </section>
+
+        <NavigationBar />
+      </div>
+    </main>
+  );
+}
+
+export default MyProgress;
